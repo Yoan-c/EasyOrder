794f0423d28a068aaa64587ada3cd0fc9201a24f
ti-metiss <coutenay.yoan@hotmail.fr>
2022-09-10 15:36:23 +0200

    ajout modal

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Produit;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,10 +12,12 @@ use Symfony\Component\Routing\Annotation\Route;
 class MainController extends AbstractController
 {
     #[Route('/', name: 'app_main')]
-    public function index(): Response
+    public function index(ManagerRegistry $doctrine): Response
     {
+        $productRepository = $doctrine->getRepository(Produit::class);
+        $products = $productRepository->findAll();
         return $this->render('main/index.html.twig', [
-            'controller_name' => 'MainController',
+            'produits' => $products,
         ]);
     }
 }
